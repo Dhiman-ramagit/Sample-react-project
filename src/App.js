dd49467a0ec2456ee9e72e000b48b157a46d18cd
Rama Dhiman <rama.dhiman@phonepe.com>
2024-04-24 17:01:24 +0530

    changed strign

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is a sample app
+          this is a sample app2
         </p>
         <a
           className="App-link"
